a075d1ce863f9bf6d01301555f1c5ab1f1563212
Vitor Emanuel Batista <vitorebatista@gmail.com>
2018-06-22 13:41:33 -0300

    create todoitem

--- server/models/todo.js
+++ server/models/todo.js
@@ -1,10 +1,17 @@
-'use strict';
 module.exports = (sequelize, DataTypes) => {
-  var Todo = sequelize.define('Todo', {
-    title: DataTypes.STRING
-  }, {});
-  Todo.associate = function(models) {
-    // associations can be defined here
+  const Todo = sequelize.define('Todo', {
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  });
+
+  Todo.associate = (models) => {
+    Todo.hasMany(models.TodoItem, {
+      foreignKey: 'todoId',
+      as: 'todoItems',
+    });
   };
+
   return Todo;
 };
