eccd65379f51ace2512acffba3e065245672293e
Vitor Emanuel Batista <vitorebatista@gmail.com>
2018-06-28 23:32:14 -0300

    ,

--- server/models/todoitem.js
+++ server/models/todoitem.js
@@ -2,18 +2,18 @@ module.exports = (sequelize, DataTypes) => {
   const TodoItem = sequelize.define('TodoItem', {
     content: {
       type: DataTypes.STRING,
-      allowNull: false,
+      allowNull: false
     },
     complete: {
       type: DataTypes.BOOLEAN,
-      defaultValue: false,
+      defaultValue: false
     },
   });
 
   TodoItem.associate = (models) => {
     TodoItem.belongsTo(models.Todo, {
       foreignKey: 'todoId',
-      onDelete: 'CASCADE',
+      onDelete: 'CASCADE'
     });
   };
 
