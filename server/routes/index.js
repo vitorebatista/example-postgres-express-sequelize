c3330a53baf1331abd061868c2422519487f1371
Vitor Emanuel Batista <vitorebatista@gmail.com>
2018-06-28 23:12:03 -0300

    delete todo

--- server/routes/index.js
+++ server/routes/index.js
@@ -10,5 +10,7 @@ module.exports = (app) => {
   app.post('/api/todos/:todoId/items', todoItemsController.create);
   app.get('/api/todos', todosController.list);
   app.get('/api/todos/:todoId', todosController.retrieve);
+  app.put('/api/todos/:todoId', todosController.update);
+  app.delete('/api/todos/:todoId', todosController.destroy);
 
 };
